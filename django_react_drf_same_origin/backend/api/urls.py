8496e15b4cea35a53600f6de1fc6b7ff95d721de
Michael Herman <hermanmu@gmail.com>
2020-12-09 18:35:41 -0600

    update django react same origin drf

--- django_react_drf_same_origin/backend/api/urls.py
+++ django_react_drf_same_origin/backend/api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
 from . import views
-from .views import SessionView, WhoAmIView
 
 urlpatterns = [
     path('csrf/', views.get_csrf, name='api-csrf'),
     path('login/', views.login_view, name='api-login'),
     path('logout/', views.logout_view, name='api-logout'),
-    path('session/', SessionView.as_view(), name='api-session'),
-    path('whoami/', WhoAmIView.as_view(), name='api-whoami'),
+    path('session/', views.SessionView.as_view(), name='api-session'),
+    path('whoami/', views.WhoAmIView.as_view(), name='api-whoami'),
 ]
